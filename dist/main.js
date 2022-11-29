afec6a5c03353a45101656aaac07bfed8de4fab0
aliobba <ali.obba@esprit.tn>
2022-11-29 16:11:04 +0100

    allow cors new url from

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: ['http://localhost:3000'],
+        origin: ['http://localhost:3000', process.env.REACT_FRONT_URI],
     });
     const config = new swagger_1.DocumentBuilder()
         .setTitle('Workshop example')
