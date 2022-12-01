363135d70dc317d15706efb6625a645d4a578699
aliobba <ali.obba@esprit.tn>
2022-12-01 17:17:34 +0100

    update auth google service

--- src/google-auth/google-auth.service.ts
+++ src/google-auth/google-auth.service.ts
@@ -5,7 +5,7 @@ import { JwtService } from '@nestjs/jwt';
 export class GoogleAuthService {
   constructor(private jwtService: JwtService) {}
 
-  googleLogin(req, res) {
+  async googleLogin(req, res) {
     if (!req.user) {
       return 'No user from google';
     }
@@ -16,12 +16,14 @@ export class GoogleAuthService {
     };
     console.log(req.user);
 
-    return res.cookie('access_token', resultat.access_token, {
+    await res.cookie('access_token', resultat.access_token, {
       maxAge: 3600 * 1000,
       domain: 'netlify.app',
       sameSite: true,
       secure: false,
-    }).redirect(process.env.REACT_FRONT_URI);
+    });
+
+    return res.redirect(process.env.REACT_FRONT_URI);
 
     // return res.json(req.user);
 
