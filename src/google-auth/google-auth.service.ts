73a1e12c4e8a41255d85f897ffb46a1de1673050
aliobba <ali.obba@esprit.tn>
2022-12-01 16:17:51 +0100

    update auth google service

--- src/google-auth/google-auth.service.ts
+++ src/google-auth/google-auth.service.ts
@@ -18,7 +18,7 @@ export class GoogleAuthService {
 
     return res.cookie('access_token', resultat.access_token, {
       maxAge: 3600 * 1000,
-      sameSite: true,
+      sameSite: 'Lax',
       secure: false,
     }).redirect(process.env.REACT_FRONT_URI);
 
