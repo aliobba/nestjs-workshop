493284b5b0dc2280cdcae13701bbeae828a6c155
aliobba <ali.obba@esprit.tn>
2022-12-02 11:50:17 +0100

    update auth google service

--- src/google-auth/google-auth.controller.ts
+++ src/google-auth/google-auth.controller.ts
@@ -15,9 +15,4 @@ export class GoogleAuthController {
   async googleAuthRedirect(@Req() req, @Res() res) {
     this.googleAuthService.googleLogin(req, res);
   }
-
-  @Get('tokenSession')
-  async getToken(@Req() req, @Res() res) {
-    this.googleAuthService.getToken(req, res);
-  }
 }
--- src/google-auth/google-auth.service.ts
+++ src/google-auth/google-auth.service.ts
@@ -17,14 +17,13 @@ export class GoogleAuthService {
     console.log(req.user);
 
     // return res.set({'Set-Cookie': 'access_token='+resultat.access_token+'; Domain=.netlify.app; SameSite=Strict'}).json(res.user);
-    return res
-      .cookie('access_token', resultat.access_token, {
-        maxAge: 3600 * 1000,
-        sameSite: 'none',
-        // httpOnly: false,
-        secure: true,
-      })
-      .redirect(process.env.REACT_FRONT_URI);
+    return res.cookie('access_token', resultat.access_token, {
+      maxAge: 3600 * 1000,
+      sameSite: "none",
+      domain: 'euphonious-biscochitos-db0def.netlify.app',
+      // httpOnly: false,
+      secure: true,
+    }).redirect(process.env.REACT_FRONT_URI);
 
     //  res.redirect(process.env.REACT_FRONT_URI);
 
@@ -36,16 +35,5 @@ export class GoogleAuthService {
       httpOnly: false,
       path: '/',
     }).redirect(process.env.REACT_FRONT_URI); */
-  }
-
-  getToken(req, res) {
-    try {
-      console.log(req.session.access_token);
-      return res.send({ message: req.session.access_token });
-    } catch (error) {
-      console.log(error);
-      res.status(500);
-      res.send(error);
-    }
   }
 }
